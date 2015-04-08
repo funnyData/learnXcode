d186e47e22d7b07bf7a027a4ad67972c7f5c7e90
Richard <njuchendong@126.com>
2015-04-08 20:25:39 +0800

    First commit

--- L04Array/main.swift
+++ L04Array/main.swift
@@ -9,4 +9,16 @@
 import Foundation
 
 println("Hello, World!")
+var arr = ["Hello", "jikexueyuan", 200, 2.4]
+println(arr[0])
+
+arr.append(1000)
+println(arr)
+
+arr.insert(50, atIndex: 2)
+println(arr)
+
+for bgen in enumerate(arr){
+    println("元素下标:\(bgen.0)  元素值:\(bgen.1)")
+}
 
